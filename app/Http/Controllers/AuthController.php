ee97109fd01586d72af4d6f75e02e96de54fe42a
Hasan Fadillah <hasan.tecnic@gmail.com>
2023-10-05 00:24:40 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect('dashboard');
+        }
+
+        return back();
+    }
+
+    public function dashboard()
+    {
+        return view('welcome');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:250',
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        Auth::attempt($credentials);
+        $request->session()->regenerate();
+
+        return redirect('/');
+    }
+}
